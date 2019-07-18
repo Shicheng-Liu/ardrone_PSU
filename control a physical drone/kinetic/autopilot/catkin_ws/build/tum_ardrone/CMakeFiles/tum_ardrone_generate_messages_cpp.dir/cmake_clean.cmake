a22ff8755c30c1c5b2b08180f2d5a846cc3d8e68
Shicheng-Liu <liushicheng.11@gmail.com>
2019-07-17 20:57:03 -0400

    autopilot upload

---
+++ control a physical drone/kinetic/autopilot/catkin_ws/build/tum_ardrone/CMakeFiles/tum_ardrone_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tum_ardrone_generate_messages_cpp"
+  "/home/user/catkin_ws/devel/include/tum_ardrone/filter_state.h"
+  "/home/user/catkin_ws/devel/include/tum_ardrone/SetReference.h"
+  "/home/user/catkin_ws/devel/include/tum_ardrone/SetInitialReachDistance.h"
+  "/home/user/catkin_ws/devel/include/tum_ardrone/SetStayTime.h"
+  "/home/user/catkin_ws/devel/include/tum_ardrone/SetStayWithinDistance.h"
+  "/home/user/catkin_ws/devel/include/tum_ardrone/SetMaxControl.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tum_ardrone_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
