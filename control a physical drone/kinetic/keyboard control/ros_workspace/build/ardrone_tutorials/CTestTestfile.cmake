de577e1c64597cdecb7f928d5ac7fdaec82c4e9d
Shicheng-Liu <liushicheng.11@gmail.com>
2019-07-19 18:45:46 -0400

    Delete

--- control a physical drone/kinetic/keyboard control/ros_workspace/build/ardrone_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/ros_workspace/src/ardrone_tutorials
-# Build directory: /home/user/ros_workspace/build/ardrone_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
