5cfc7d35c8ebe202996a4cb2caf7843326f0fbc1
Shicheng-Liu <liushicheng.11@gmail.com>
2019-07-19 17:47:54 -0400

    add

---
+++ control a physical drone/kinetic/autopilot/catkin_ws/build/tum_ardrone/CMakeFiles/tum_ardrone_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tum_ardrone_generate_messages_eus"
+  "/home/user/catkin_ws/devel/share/roseus/ros/tum_ardrone/msg/filter_state.l"
+  "/home/user/catkin_ws/devel/share/roseus/ros/tum_ardrone/srv/SetReference.l"
+  "/home/user/catkin_ws/devel/share/roseus/ros/tum_ardrone/srv/SetInitialReachDistance.l"
+  "/home/user/catkin_ws/devel/share/roseus/ros/tum_ardrone/srv/SetStayTime.l"
+  "/home/user/catkin_ws/devel/share/roseus/ros/tum_ardrone/srv/SetStayWithinDistance.l"
+  "/home/user/catkin_ws/devel/share/roseus/ros/tum_ardrone/srv/SetMaxControl.l"
+  "/home/user/catkin_ws/devel/share/roseus/ros/tum_ardrone/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tum_ardrone_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
