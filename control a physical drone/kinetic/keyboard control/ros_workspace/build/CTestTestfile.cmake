cece7981b97c9a5b695f3e22f2b83488b8a68a84
Shicheng-Liu <liushicheng.11@gmail.com>
2019-07-19 17:42:58 -0400

    try

--- control a physical drone/kinetic/keyboard control/ros_workspace/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/ros_workspace/src
-# Build directory: /home/user/ros_workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(ardrone_tutorials)
