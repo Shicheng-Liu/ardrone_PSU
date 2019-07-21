03b6fa36de9434e4a4ea31cee8d9d71689a7fb53
Shicheng-Liu <liushicheng.11@gmail.com>
2019-07-21 12:59:47 -0400

    revise

--- control a physical drone/indigo/keyboard control/ros_workspace/build/ardrone_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/server/ros_workspace/src/ardrone_tutorials
-# Build directory: /home/server/ros_workspace/build/ardrone_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
